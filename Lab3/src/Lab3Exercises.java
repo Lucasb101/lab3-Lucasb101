c004d03b449ff86b54a1f0cefdbfbd4497d5e76b
3010a1 <3010a1@china.ece.uprm.edu>
2017-02-08 16:34:48 -0400

    Changed name and set to open at center of page

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,11 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Lucas");
+		JFrame myFrame = new JFrame("Lucas ");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 500);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
-		
 	}
 }
